792f23e2bb1256041ced2639a72f7d287b5c59ec
guinleng <guinleng@users.noreply.github.com>
2025-11-13 12:55:16 +0000

    Add build-handler-statistics.less, complex-api.rs

---
+++ frontend/web/src/subscriptions/types/fixture/complex-api.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/langlearn_fhe/frontend/web/src/subscriptions/types/fixture/complex-api.rs
+// Generated at: 2025-11-27T12:25:21.439Z
+
+
